997a7c32dcf6a4a6bdcd864552716aaa00134327
Daniel Munger and Skye Atwood <skye@>
2016-10-26 11:15:02 -0700

    add function to add ay onto end of word starting with vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 //Back-End
 var original;
+var output = [];
 var letters = [];
 var byLetter ="";
 var letterCheck;
+var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"]
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 var splitString = function(stringToSplit, separator) {
   return letters = stringToSplit.split(separator);
 };
-var letterCheck = function(characters, arrayToCheck) {
-  for(index=0; index<characters.length; index++) {
-    for(i=0; i<arrayToCheck.length; i++) {
-      if(characters[index] === arrayToCheck[i]){
-        alert("it works");
+var vowelCheck = function(words, vowel) {
+    for(index=0; index<vowel.length; index++){
+      if(words[0] === vowel[index]) {
+        words.push("ay")
+        output = words.join("");      
       }
-      else{
-        alert("it doesn't work");
-      }
+      else{}
     }
-  };
 };
+
 
 //Front-End
 $(document).ready(function() {
@@ -27,7 +27,8 @@ $(document).ready(function() {
     event.preventDefault();
     original = $("#sentence").val();
     letters = splitString(original, byLetter);
-    letterCheck(letters, alphabet);
+    vowelCheck(letters, vowels);
+    //letterCheck(letters, alphabet);
 
   });
 
