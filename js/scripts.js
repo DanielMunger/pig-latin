169f8111fc3845774511639d48fde83ee3f535f5
Daniel Munger and Skye Atwood <skye@>
2016-10-26 15:23:57 -0700

    add javascript function to deal woth qu words.

--- js/scripts.js
+++ js/scripts.js
@@ -33,17 +33,23 @@ for(i=0; i<symbols.length; i++) {
 var consonantCheck = function(words, vowel) {
   for(i=0; i<words.length; i++) {
     for(index=0; index<vowel.length; index++) {
-      if(words[i] === vowel[index]) {
-        console.log(words)
-        split = words.splice(0,i);
-        console.log(split, words);
+      if(words[0]===('q'||'Q') && words[1] === ('u' || 'U')) {
+        split = words.splice(0,2);
         split = split.join('');
         words.push(split);
         joined = words.join('');
         words.push("ay");
         output = words.join('');
-        console.log(output);
         return output;
+      }
+      else if(words[i] === vowel[index]) {
+          split = words.splice(0,i);
+          split = split.join('');
+          words.push(split);
+          joined = words.join('');
+          words.push("ay");
+          output = words.join('');
+          return output;
       }
     }
   }
